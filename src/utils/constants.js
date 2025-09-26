ed165470dfcf5fbf515047b502e9b5e6f88eab16
SterziPaolo <sterzipaolo807@gmail.com>
2025-09-26 11:35:24 -0700

    update "modal"

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const STATIC_URL = [
 ]
 
 export const TEXT_INFORMATION = [
-    "Hi Sterzi, This is the text we can insert in the information box of each game: Carica i dati del team, definisci il job profile, aggiungi i CV dei candidati e lascia che l'AI analizzi ogni profilo fornendo valutazioni oggettive su Job Fit, Team Fit e raccomandazioni finali.(Matcho)",
-    "Hi Sterzi, This is the text we can insert in the information box of each game: Scegli i tuoi obiettivi, affronta decisioni lavorative realistiche e scopri come le tue scelte plasmano il tuo profilo professionale. Al termine ricevi un'analisi personalizzata delle tue competenze e attitudini. (The Sims)",
-    "Hi Sterzi, This is the text we can insert in the information box of each game: Le tue scelte verranno analizzate per misurare: risk taking, pensiero strategico, gestione risorse, velocità decisionale (Monopoly)",
+    "Carica i dati del team, definisci il job profile, aggiungi i CV dei candidati e lascia che l'AI analizzi ogni profilo fornendo valutazioni oggettive su Job Fit, Team Fit e raccomandazioni finali.(Matcho)",
+    "Scegli i tuoi obiettivi, affronta decisioni lavorative realistiche e scopri come le tue scelte plasmano il tuo profilo professionale. Al termine ricevi un'analisi personalizzata delle tue competenze e attitudini. (The Sims)",
+    "Le tue scelte verranno analizzate per misurare: risk taking, pensiero strategico, gestione risorse, velocità decisionale (Monopoly)",
 ]
